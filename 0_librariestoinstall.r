4edeb62608b9f03bb061ff6c6ae0f5812fc980e2
Shikhar Srivastava <shikhar.srivastava@outlook.com>
2016-06-28 11:00:13 +0200

    updated

--- 0_librariestoinstall.r
+++ 0_librariestoinstall.r
@@ -1,20 +1,15 @@
 install.packages("caret", repos='http://cran.us.r-project.org')
-install.packages("gbm", repos='http://cran.us.r-project.org')
 install.packages("nnet", repos='http://cran.us.r-project.org')
 install.packages("pROC", repos='http://cran.us.r-project.org')
-install.packages("hmeasure", repos='http://cran.us.r-project.org')
 install.packages("randomForest", repos='http://cran.us.r-project.org')
-install.packages("elmNN", repos='http://cran.us.r-project.org')
 install.packages("doSNOW", repos='http://cran.us.r-project.org')
 install.packages("foreach", repos='http://cran.us.r-project.org')
 install.packages("stepPlr", repos='http://cran.us.r-project.org')
-install.packages("e1071", repos='http://cran.us.r-project.org')
 install.packages("plyr", repos='http://cran.us.r-project.org')
 install.packages("ROCR", repos='http://cran.us.r-project.org')
 install.packages("compiler", repos='http://cran.us.r-project.org')
 install.packages("R.utils", repos='http://cran.us.r-project.org')
 install.packages("verification", repos='http://cran.us.r-project.org')
-install.packages("RCurl", repos='http://cran.us.r-project.org')
 
 
 
